4893c36582d37052aa0694eb6cebb28f5dfa4833
Ross Harrison <rtharrison86@gmail.com>
2016-01-18 01:31:19 -0500

    print out fields in json

--- source/app.d
+++ source/app.d
@@ -1,6 +1,18 @@
 import std.stdio;
+import std.json;
+import JSONInflater;
 
 void main()
 {
-	writeln("Edit source/app.d to start your project.");
+  TestObj to;
+  auto json_s = `{"id": "7", "name": "Really cool Guy"}`;
+  auto json_v = std.json.parseJSON(json_s);
+  auto t = typeof(to).stringof;
+
+  JSONInflater.Marshall(to, json_v);
+}
+
+class TestObj{
+  string id;
+  string name;
 }
