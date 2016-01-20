53d9371126981086d09e8364cbb445f14b9b5ec2
Ross Harrison <rtharrison86@gmail.com>
2016-01-20 01:36:34 -0500

    handle nested objects

--- source/JsonInflater.d
+++ source/JsonInflater.d
@@ -21,18 +21,26 @@ void Unmarshall(T)(ref T obj, in JSONValue json){
     alias fieldType = typeof(__traits(getMember, obj, k));
     auto jsonField = json[k];
 
-    switch(jsonField.type){
-      case JSON_TYPE.INTEGER:
-        __traits(getMember, obj, k) = to!(fieldType)(jsonField.integer);
-        break;
-      case JSON_TYPE.FLOAT:
-        __traits(getMember, obj, k) = to!(fieldType)(jsonField.floating);
-        break;
-      case JSON_TYPE.STRING:
-       __traits(getMember, obj, k) = to!(fieldType)(jsonField.str);
-        break;
-      default:
-        writefln("Don't know how to handle: %s", jsonField.type);
+    static if(isBasicType!fieldType || isNarrowString!fieldType){
+
+      switch(jsonField.type){
+        case JSON_TYPE.INTEGER:
+          __traits(getMember, obj, k) = to!(fieldType)(jsonField.integer);
+          break;
+        case JSON_TYPE.FLOAT:
+          __traits(getMember, obj, k) = to!(fieldType)(jsonField.floating);
+          break;
+        case JSON_TYPE.STRING:
+         __traits(getMember, obj, k) = to!(fieldType)(jsonField.str);
+          break;
+        default:
+          writefln("Don't know how to handle: %s", jsonField.type);
+      }
+
+    } else {
+      auto child = new fieldType;
+      JSONInflater.Unmarshall(child, jsonField);
+      __traits(getMember, obj, k) = child;
     }
   }
 }
@@ -46,7 +54,15 @@ JSONValue* Marshall(T)(in T inObj){
   
   // create map from name to type
   foreach(k; fnt){
-    outJson.object[k] = JSONValue(__traits(getMember, inObj, k));
+    alias fieldType = typeof(__traits(getMember, inObj, k));
+
+    static if(isBasicType!fieldType || isNarrowString!fieldType){
+      outJson.object[k] = JSONValue(__traits(getMember, inObj, k));
+    } else { // assume object
+      writeln("doing i peter");
+      auto child = new fieldType;
+      outJson.object[k] = JSONInflater.Marshall!(fieldType)(child).object;
+    }
   }
 
   return outJson;
